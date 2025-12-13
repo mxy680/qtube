5e6f9d998ba287e7db9971477e0fe3ccc4202cdc
Mark Shteyn <markshteyn1@gmail.com>
2025-12-13 08:11:15 -0500

    Add Google OAuth option to signup page

--- lib/auth.ts
+++ lib/auth.ts
@@ -9,6 +9,12 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
   baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
   basePath: "/api/auth",
 })
